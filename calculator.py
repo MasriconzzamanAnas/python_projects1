762deb9f8bc7c87e6dc7df9b8f7c5826ca113ad2
Masricozzaman Anas <153457604+MasriconzzamanAnas@users.noreply.github.com>
2024-09-14 21:21:12 +0600

    main

---
+++ calculator.py
@@ -0,0 +1,44 @@
+def calculator():
+    while True:
+        print('\nSelect operation: ')
+        print('1. Add')
+        print('2. Subtract')
+        print('3. Multiply')
+        print('4. Divide')
+        print('5. Modulus')
+        print('6. Exit ')
+        opera = int(input('Enter choice (1/2/3/4/5/6):'))
+
+        if opera == 6:
+            print("Exiting the calculator. Goodbye!")
+            break
+
+        if opera in [1,2,3,4,5]:
+            try:
+                nu1 = float(input("Enter first number: "))
+                nu2 = float(input("Enter second number: "))
+            except ValueError:
+                print("Invalid input! Please enter numerical values.")
+                continue
+
+        if opera == 1:
+            print(f"Result {nu1} + {nu2} = {nu1 + nu2}")
+        elif opera == 2:
+            print(f"Result: {nu1} - {nu2} = {nu1 - nu2}")
+        elif opera == 3:
+            print(f"Result {nu1} * {nu2} = {nu1 * nu2}")
+        elif opera == 4:
+            if nu1 !=0 and nu2 != 0:
+                print(f"Result: {nu1} / {nu2} = {nu1 / nu2}")
+            else:
+                print("Error: Cannot divide by zero.")
+        elif opera == 5:
+            if nu2 != 0 and nu1 !=0:
+                print(f"Result: {nu1} % {nu2} = {nu1 % nu2}")
+            else:
+                print("Error: Cannot Modulus by zero.")
+        else:
+            print("Invalid choice! Please select a valid operation.")
+
+
+calculator()
---
+++ num.py
@@ -0,0 +1,26 @@
+import random
+
+def number_guessing_game():
+
+    try:
+        randomNumber= random.randint(1,100)
+        attempt= 0
+
+
+        while True:
+            gauss = int(input('Input your gauss number: '))
+            attempt += 1
+
+            if randomNumber > gauss:
+                print('Too Low')
+            elif gauss > randomNumber:
+                print("Too High")
+            else:
+                print(f"Congratulations! You've guessed the number in {attempt} attempts.")
+                break
+
+    except ValueError:
+        print("Invalid input! Please enter a valid number.")
+
+
+number_guessing_game()
