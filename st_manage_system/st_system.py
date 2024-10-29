70eb2e6c32a13cf556be18ef071a5ccf44a5c882
Masricozzaman Anas <153457604+MasriconzzamanAnas@users.noreply.github.com>
2024-10-29 16:01:09 +0600

    mani

--- st_manage_system/st_system.py
+++ st_manage_system/st_system.py
@@ -55,7 +55,56 @@ class Course:
 
     
 #Section B (system menagement)
+class Student_management_System:
+    def __init__(self):
+        self.students= {}
+        self.course = {}
 
+    def add_student(self, name, age, address, student_id):
+        if student_id is self.students:
+            print('The student alrady exists')
+        else:
+            student= self.students(name,age,address, student_id)
+            self.students[student_id] = student
+            print(f"Student {name} (student_id: {student_id}) add successfuly")
+
+    def add_course(self, courseName, courseCode, instructor):
+        if courseCode in self.course:
+            print('The course already exists')
+        else:
+            course = self.course(courseName, courseCode, instructor)
+            self.course[courseCode] = course
+            print(f"Course {courseName} (course_id: {courseCode})  create with the Instructor {instructor}")
+
+    def enroll_student_in_course(self, courser_coud, studentId):
+        # check if student and course exists in the system before enrolling them in the course.
+        students= self.students.get(studentId)
+        course = self.course.get(courser_coud)
+
+        if students and course:
+            students.enroll_course(course.courseName)
+            course.add_student(students)
+            print(f"Student {studentId} enrolled successfully in Course {course.courseName} course code: {courser_coud}")
+        else:
+            print('Either student or course does not exist')
+
+    def add_great_for_student(self, student_id, courseCode, great):
+        # check if student and course exists in the system before enrolling them in the course
+        student = self.students.get(student_id)
+        course = self.course.get(courseCode)
+
+        if student_id and course:
+            student.add_grades(course.courseName, great)
+            print(f"Great added to student {student_id} for the {course.courseName}")
+        else:
+            print('Either student or course does not exist')
+
+    def display_student_Details(self, student_id):
+        student = self.students.get(student_id)
+        if student:
+            student.show_student_info()
+        else:
+            print(f'Student with id {student_id} does not exist')
 
 
 
