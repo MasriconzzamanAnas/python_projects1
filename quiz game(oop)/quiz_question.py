6ce50f9ac86b17db9037ddb7cad62908e2c88965
Masricozzaman Anas <153457604+MasriconzzamanAnas@users.noreply.github.com>
2024-11-26 11:24:31 +0600

    main

---
+++ quiz game(oop)/quiz_question.py
@@ -0,0 +1,22 @@
+question_bank = [
+    {"text":"Python is a statically-typed programming language.", "answer": "False"},
+    {"text":"The Earth is the third planet from the Sun." "True"},
+    {"text":"HTML stands for Hyper Text Markup Language." "True"},
+    {"text":"The boiling point of water is 50 degrees Celsius.", "answer": "False"},
+    {"text":"The keyword 'class' is used to define a class in Python.", "answer": "True"},
+    {"text":"React.js is a backend framework for building servers.,", "answer": "False"},
+    {"text":"Saturn is the largest planet in our solar system.", "answer": "False"},
+    {"text":"The 'def' keyword in Python is used to define a function.", "answer": "True"},
+    {"text":"The capital of France is Berlin.", "answer": "False"},
+    {"text":"JavaScript can be used for both frontend and backend development.", "answer": "True"},
+    {"text":"In Python, lists are immutable data types.", "answer": "False"},
+    {"text":"The Great Wall of China is visible from space.", "answer": "False"},
+    {"text":"Binary code consists of 1s and 0s.", "answer": "True"},
+    {"text":"The 'for' loop in Python can iterate over lists, strings, and tuples.", "answer": "True"},
+    {"text":"CSS stands for Cascading Style Sheets.", "answer": "True"},
+    {"text":"The Eiffel Tower is located in London.", "answer": "False"},
+    {"text":"MySQL is a relational database management system.", "answer": "True"},
+    {"text":"Electricity is measured in units of amperes.", "answer": "False"},  # It's measured in volts.
+    {"text":"Linux is an open-source operating system.", "answer": "True"},
+    {"text":"In object-oriented programming, inheritance allows a class to inherit properties and methods from another class.", "answer": "True"}
+]
